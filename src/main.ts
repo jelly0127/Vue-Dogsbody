be84dbb9d34d6783e39e725e5edd6a3189238fb7
Demons <oncwnuA-Qman3Lxb6_AFTz4gOqEU@git.weixin.qq.com>
2023-04-22 23:23:37 +0800

    feat: test lint

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
+import 'virtual:uno.css'
 import './assets/main.css'
 const app = createApp(App)
 app.use(ElementPlus, { size: 'small', zIndex: 3000 })
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path'
+import UnoCSS from 'unocss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,7 +18,8 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
-    })
+    }),
+    UnoCSS()
   ],
   resolve: {
     alias: {
